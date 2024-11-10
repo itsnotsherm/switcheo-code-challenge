866a5472c7974b81cfadf7d11f114039a83f3e52
root <root@LAPTOP-ENTRRQV5>
2024-11-11 01:38:33 +0800

    Add Create and Update post functionality

---
+++ src/Problem 5/blog/x/blog/keeper/msg_server_update_post.go
@@ -0,0 +1,30 @@
+package keeper
+
+import (
+	"context"
+
+	"blog/x/blog/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k msgServer) UpdatePost(goCtx context.Context, msg *types.MsgUpdatePost) (*types.MsgUpdatePostResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Handling the message
+	var post = types.Post{
+		Creator: msg.Creator,
+		Id: msg.Id,
+		Title: msg.Title,
+		Body: msg.Body,
+	}
+	val, found := k.GetPost(ctx, msg.Id)
+	if !found {
+		return nil, errorsmod.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
+	}
+	if msg.Creator != val.Creator {
+		return nil, errorsmod.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
+	k.SetPost(ctx, post)
+	return &types.MsgUpdatePostResponse{}, nil
+}
